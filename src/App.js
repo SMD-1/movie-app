dba8643b4a2834fc2da5968d0115ec444ed85133
Danish <skdanish1999@gmail.com>
2021-08-03 16:04:32 +0530

    search movie

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { useState, useEffect } from "react";
 import Movie from "./Component/Movie";
 import "./App.css";
-import Navbar from "./Component/Navbar";
+import { FaGithub } from "react-icons/fa";
 
 const FEATURED_API =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2d248459fa33105bff866bf17b092b83&page=1";
 
-// const SEARCH_API =
-//   "https://api.themoviedb.org/3/search/movie?&api_key=2d248459fa33105bff866bf17b092b83&query=";
-
-function App() {
+const App = () => {
   const [movies, setMovies] = useState([]);
   // eslint-disable-next-line
   useEffect(async () => {
@@ -20,10 +17,44 @@ function App() {
         setMovies(data.results);
       });
   }, []);
+
+  const SEARCH_API =
+    "https://api.themoviedb.org/3/search/movie?&api_key=2d248459fa33105bff866bf17b092b83&query=";
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    fetch(SEARCH_API + searchTerm)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setMovies(data.results);
+      });
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div className="container">
-      <div className="headerContainer">
-        <Navbar />
+      <div className="navbar">
+        <form onSubmit={handleOnSubmit}>
+          <div className="search">
+            <input
+              type="search"
+              placeholder="search"
+              value={searchTerm}
+              onChange={handleOnChange}
+            />
+          </div>
+        </form>
+        <div className="github">
+          <a href="https://github.com/SMD-1/movie-app">
+            <FaGithub color="white" size="2.5rem" />
+          </a>
+        </div>
       </div>
       <div id="movies">
         {movies.length > 0 &&
@@ -33,6 +64,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
